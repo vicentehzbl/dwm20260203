501e3f312f6425ca68299d325b5243f273bb10ad
vicentehzbl <v.hormazbalpacheco@uandresbello.edu>
2026-08-16 23:57:21 -0400

    Cambios en index y cambio de imagen chicago.jpg

--- index.php
+++ index.php
@@ -5,13 +5,25 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>        
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
+         <!--Estructura Footer-->
+        <style>
+            body {
+                min-height: 100vh;
+                display: flex;
+                flex-direction: column;
+            }
+
+            .footer {
+                margin-top: auto;
+            }
+        </style>        
     </head>
     <body>
         <!-- Navbar -->
         <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
             <div class="container-fluid">
-                <a class="navbar-brand" href="index.php">Logo</a>
+                <a class="navbar-brand" href="index.php">VICENTE HORMAZÁBAL PACHECO</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
                 <span class="navbar-toggler-icon"></span>
                 </button>        
@@ -39,6 +51,75 @@
                 <button type="button" class="btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#myModal">Acceder</button>             
             </div>         
         </nav>
+        <!-- Carousel -->
+        <div id="demo" class="carousel slide" data-bs-ride="carousel">
+
+        <!-- Indicators/dots -->
+            <div class="carousel-indicators">
+
+                <button type="button"
+                        data-bs-target="#demo"
+                        data-bs-slide-to="0"
+                        class="active">
+                </button>
+
+                <button type="button"
+                        data-bs-target="#demo"
+                        data-bs-slide-to="1">
+                </button>
+
+                <button type="button"
+                        data-bs-target="#demo"
+                        data-bs-slide-to="2">
+                </button>
+
+            </div>
+
+        <!-- Imágenes -->
+            <div class="carousel-inner">
+
+                <div class="carousel-item active">
+                    <img src="img/la.jpg"
+                        alt="Los Angeles"
+                        class="d-block w-100">
+                </div>
+
+                <div class="carousel-item">
+                    <img src="img/chicago.jpg"
+                        alt="Chicago"
+                        class="d-block w-100">
+                </div>
+
+                <div class="carousel-item">
+                    <img src="img/ny.jpg"
+                        alt="New York"
+                        class="d-block w-100">
+                </div>
+
+            </div>
+
+        <!-- Botón anterior -->
+            <button class="carousel-control-prev"
+                    type="button"
+                    data-bs-target="#demo"
+                    data-bs-slide="prev">
+
+                <span class="carousel-control-prev-icon"></span>
+
+            </button>
+
+        <!-- Botón siguiente -->
+            <button class="carousel-control-next"
+                    type="button"
+                    data-bs-target="#demo"
+                    data-bs-slide="next">
+
+                <span class="carousel-control-next-icon"></span>
+
+            </button>
+
+        </div>
+        
         <!-- Container -->
         <div class="container-fluid bg-warning">
             Hola Principal<br>
@@ -49,10 +130,10 @@
             <a href="contacto.php">Ir a Contacto</a><br>
         </div>
         <!-- Footer -->
-        <div class="container-fluid bg-dark">
+        <div class="container-fluid bg-dark footer">
             <div class="row">
                 <div class="col-4"></div>
-                <div class="col-4" style="color:white"><strong>MiEmpresa@2026</strong></div>
+                <div class="col-4 text-center" style="color:white"><strong>MiEmpresa@2026</strong></div>
                 <div class="col-4"></div>
             </div>
         </div>
